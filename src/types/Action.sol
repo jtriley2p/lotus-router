8597a3e9675988d8f737f007bcf0f0b957fe4639
jtriley <jtriley15@gmail.com>
2025-02-22 17:25:04 -0800

    minor tweaks

--- src/types/Action.sol
+++ src/types/Action.sol
@@ -1,19 +1,16 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.19;
 
-import { BBCDecoder } from "src/types/BBCDecoder.sol";
+import { BBCDecoder } from "src/util/BBCDecoder.sol";
 import { BytesCalldata } from "src/types/BytesCalldata.sol";
 import { Ptr } from "src/types/PayloadPointer.sol";
 import { UniV2Pair } from "src/types/protocols/UniV2Pair.sol";
 
 enum Action {
     Halt,
-    UniV2SwapExactIn,
-    UniV2SwapExactOut,
-    UniV3SwapExactIn,
-    UniV3SwapExactOut,
-    UniV4SwapExactIn,
-    UniV4SwapExactOut,
+    UniV2Swap,
+    UniV3Swap,
+    UniV4Swap,
     ERC20Transfer,
     ERC20TransferFrom,
     ERC721TransferFrom,
@@ -26,16 +23,17 @@ enum Action {
 using { execute } for Action global;
 
 function execute(Action action, Ptr ptr) returns (Ptr, bool success) {
-    if (action == Action.UniV2SwapExactIn) {
+    if (action == Action.UniV2Swap) {
+        bool canFail;
         UniV2Pair pair;
         uint256 amount0Out;
         uint256 amount1Out;
         address to;
         BytesCalldata data;
 
-        (ptr, pair, amount0Out, amount1Out, to, data) = BBCDecoder.decodeSwapUniv2(ptr);
+        (ptr, canFail, pair, amount0Out, amount1Out, to, data) = BBCDecoder.decodeSwapUniv2(ptr);
 
-        success = pair.swap(amount0Out, amount1Out, to, data);
+        success = canFail || pair.swap(amount0Out, amount1Out, to, data);
     } else {
         success = false;
     }
